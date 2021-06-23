74e79db2d149473ceb8f945f0ffab809a1741224
twinh <twinhuang@qq.com>
2021-06-23 16:07:13 +0800

    feat(money): 增加 `money` 方法用于计算金额

---
+++ index.js
@@ -0,0 +1,46 @@
+import currency from 'currency.js';
+
+const toCurrency = (value) => {
+  return value instanceof Money ? value.value : value;
+};
+
+class Money {
+  value;
+
+  constructor(value = 0) {
+    this.value = currency(value);
+  }
+
+  add(value) {
+    this.value = this.value.add(toCurrency(value));
+    return this;
+  }
+
+  sub(value) {
+    this.value = this.value.subtract(toCurrency(value));
+    return this;
+  }
+
+  mul(value) {
+    this.value = this.value.multiply(toCurrency(value));
+    return this;
+  }
+
+  div(value) {
+    this.value = this.value.divide(toCurrency(value));
+    return this;
+  }
+
+  toString() {
+    return this.value.toJSON().toString();
+  }
+}
+
+const money = (value) => {
+  if (value instanceof Money) {
+    return value;
+  }
+  return new Money(value);
+};
+
+export default money;
